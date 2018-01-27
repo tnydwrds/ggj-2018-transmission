2b629f2c46e0341b1471fca084022838138d0abf
Tony Edwards <tnydwrds@gmail.com>
2018-01-27 14:23:45 -0800

    Add ability to shoot

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    [SerializeField]
+    private Transform projectileSpawnLocation;
+
+    [SerializeField]
+    private GameObject projectile;
+
+    [SerializeField]
+    private float fireRate = 0.3f;
+
+    private float nextFireTime = 0f;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime) {
+            nextFireTime = Time.time + fireRate;
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 6f;
+
+    void Update() {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+}
