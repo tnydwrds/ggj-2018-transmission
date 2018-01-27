8aaaeb1b3eed7f96a911588087b78234edd57fe3
Tony Edwards <tnydwrds@gmail.com>
2018-01-27 11:02:23 -0800

    Add basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed = 3f;
+
+    void Update() {
+        // Movement
+        Vector3 movement = new Vector3();
+
+        if (Input.GetKey(KeyCode.W)) {
+            movement = Vector3.up;
+        } else if (Input.GetKey(KeyCode.A)) {
+            movement = Vector3.left;
+        } else if (Input.GetKey(KeyCode.S)) {
+            movement = Vector3.down;
+        } else if (Input.GetKey(KeyCode.D)) {
+            movement = Vector3.right;
+        }
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime);
+    }
+
+}
