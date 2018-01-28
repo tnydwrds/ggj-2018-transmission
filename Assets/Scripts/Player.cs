a8f6bfb182cf0cbb93aa85e814593e0d2027d323
Tony Edwards <tnydwrds@gmail.com>
2018-01-28 13:23:33 -0800

    Use Vector2 instead of Vector3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField]
+    private float moveSpeed = 3f;
+
+    [SerializeField]
     private GameObject currentOrb;
 
     [SerializeField]
     private GameObject nextOrb;
 
     private float orbOffsetDistance = 0.75f;
+    private Vector2 velocity = Vector2.zero;
 
     void Start() {
         SetLaunchPosition();
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) {
+            velocity = Vector2.up * moveSpeed;
+        }
+
+        if (velocity != Vector2.zero) {
+            transform.Translate(velocity * Time.deltaTime);
+        }
     }
 
     void SetLaunchPosition() {
         transform.position = currentOrb.transform.position;
 
         // Look at netx orb
-        Vector3 positionDelta = nextOrb.transform.position - transform.position;
+        Vector2 positionDelta = nextOrb.transform.position - transform.position;
         float zAngle = Mathf.Atan2(positionDelta.y, positionDelta.x) * 180 / Mathf.PI;
         transform.rotation = Quaternion.Euler(0, 0, zAngle - 90);
 
