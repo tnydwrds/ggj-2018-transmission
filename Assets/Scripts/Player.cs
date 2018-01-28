0338135f1add2d3912934663883808d82a061d0b
Tony Edwards <tnydwrds@gmail.com>
2018-01-28 13:03:04 -0800

    Setup player in launch position pointing to next orb

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject currentOrb;
+
+    [SerializeField]
+    private GameObject nextOrb;
+
+    private float orbOffsetDistance = 0.75f;
+
+    void Start() {
+        SetLaunchPosition();
+    }
+
+    void SetLaunchPosition() {
+        transform.position = currentOrb.transform.position;
+
+        // Look at netx orb
+        Vector3 positionDelta = nextOrb.transform.position - transform.position;
+        float zAngle = Mathf.Atan2(positionDelta.y, positionDelta.x) * 180 / Mathf.PI;
+        transform.rotation = Quaternion.Euler(0, 0, zAngle - 90);
+
+        transform.Translate(orbOffsetDistance * Vector3.up);
+    }
+
+}
