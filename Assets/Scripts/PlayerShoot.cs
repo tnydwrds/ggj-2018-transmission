2296ca6c4aaef529310c57cbbc8cc1c8077e6fc8
Tony Edwards <tnydwrds@gmail.com>
2018-01-27 18:42:35 -0800

    Add a projectile depot

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -8,17 +8,30 @@ public class PlayerShoot : MonoBehaviour {
     private Transform projectileSpawnLocation;
 
     [SerializeField]
-    private GameObject projectile;
-
-    [SerializeField]
     private float fireRate = 0.3f;
 
+    private GameObject projectile;
     private float nextFireTime = 0f;
 
     void Update() {
-        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime) {
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFireTime && projectile) {
             nextFireTime = Time.time + fireRate;
             Instantiate(projectile, transform.position, transform.rotation);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "ProjectileDepot") {
+            ProjectileDepot depot = other.GetComponent<ProjectileDepot>();
+            if (depot) {
+                projectile = depot.GetProjectileGameObject();
+            }
+        }
+    }
+
+    void OnTiggerExit2D(Collider2D other) {
+        if (other.tag == "ProjectileDepot") {
+            projectile = null;
         }
     }
 
---
+++ Assets/Scripts/ProjectileDepot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDepot : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject projectile;
+
+    public GameObject GetProjectileGameObject() {
+        return projectile;
+    }
+
+}
