45f6247e7da28a4be678d586725e22edf741cc8a
Tony Edwards <tnydwrds@gmail.com>
2018-01-27 20:12:36 -0800

    Add projectile bank

---
+++ Assets/Scripts/ProjectileBank.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBank : MonoBehaviour {
+
+    private SpriteRenderer overlay;
+    private float colorDecrementorAmount = 0.2f;
+
+    void Start() {
+        overlay = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Projectile") {
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+
+            Color color = overlay.color;
+            if (color.a >= 0) {
+                color.a -= colorDecrementorAmount;
+                this.overlay.color = color;
+            }
+        }
+    }
+
+}
